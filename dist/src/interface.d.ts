c1fb72e2b60d12c49f06b14864140c191905009b
Adnan Saify <adnansaify11@gmail.com>
2023-04-11 15:42:22 +0530

    updated country list and interface

--- dist/src/interface.d.ts
+++ dist/src/interface.d.ts
@@ -1,8 +1,21 @@
+export interface Timezones {
+    zoneName: string;
+    gmtOffset: number;
+    gmtOffsetName: string;
+    abbreviation: string;
+    tzName: string;
+}
 export interface ICountry {
-    id: string;
     name: string;
     phonecode: string;
-    sortname: string;
+    isoCode: string;
+    flag: string;
+    currency: string;
+    latitude: string;
+    longitude: string;
+    timezones?: Timezones[];
+    getAllCountries?(): ICountry[];
+    getCountryByCode?(): ICountry;
 }
 export interface IState {
     id: string;
--- src/interface.ts
+++ src/interface.ts
@@ -1,30 +1,30 @@
 export interface Timezones {
-    zoneName: string;
-    gmtOffset: number;
-    gmtOffsetName: string;
-    abbreviation: string;
-    tzName: string;
-  }
-  export interface ICountry {
-    name: string;
-    phonecode: string;
-    isoCode: string;
-    flag: string;
-    currency: string;
-    latitude: string;
-    longitude: string;
-    timezones?: Timezones[];
-    getAllCountries?(): ICountry[];
-    getCountryByCode?(): ICountry;
-  }
+  zoneName: string;
+  gmtOffset: number;
+  gmtOffsetName: string;
+  abbreviation: string;
+  tzName: string;
+}
+export interface ICountry {
+  name: string;
+  phonecode: string;
+  isoCode: string;
+  flag: string;
+  currency: string;
+  latitude: string;
+  longitude: string;
+  timezones?: Timezones[];
+  getAllCountries?(): ICountry[];
+  getCountryByCode?(): ICountry;
+}
 
 export interface IState {
-    id: string;
-    name: string;
-    country_id: string;
+  id: string;
+  name: string;
+  country_id: string;
 }
 export interface ICity {
-   id: string;
-   name: string;
-   state_id: string;
+  id: string;
+  name: string;
+  state_id: string;
 }
